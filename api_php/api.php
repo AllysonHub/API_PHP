48d0c39c8e3f736e78db43bff851d188cf324428
AllysonHub <allysonnrocha02@gmail.com>
2026-04-16 09:37:53 -0300

    sincro_16_04

--- api_php/api.php
+++ api_php/api.php
@@ -3,12 +3,31 @@
 
     $metodo = $_SERVER['REQUEST_METHOD']; 
 
+    // vetor indexado do php
+    $usuarios = [
+        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+        ["id" => 2, "nome" => "joão Silva", "email" => "joao@email.com"]
+        
+    ];
+
     switch ($metodo) {
         case 'GET':
-            echo "AQUI AÇÔES DO MÈTODO GET";
+            echo json_encode($usuarios);
             break;
         case 'POST':
-            echo "AQUI AÇÕES DO MÉTODO POST";
+            // echo "AQUI AÇÕES DO MÉTODO POST";
+            $dados = json_decode(file_get_contents('php://input'), true);
+            // print_r($dados);
+            $novousuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+
+            array_push($usuarios, $novousuario);
+            echo json_encode('Usuario inserido com Sucesso!!');
+            print_r($usuarios);
+            
             break;
         default:
             echo "MÉTODO NÃO ENCONTRADO!";
@@ -18,12 +37,8 @@
 
     // echo "Método da requisiçao: " . $metodo;
 
-    // $usuarios = [
-    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-    //     ["id" => 2, "nome" => "joão Silva", "email" => "joao@email.com"]
-        
-    // ];
+    
 
-    // echo json_encode($usuarios)
+    
 
 ?>
